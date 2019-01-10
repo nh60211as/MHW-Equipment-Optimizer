cd690085ba4273c09d936fc21714076b5cad4cc6
nh60211as <nh60211@gmail.com>
2019-01-10 22:47:46 +0800

    More GUI.

--- MHW Equipment Optimizer/src/equipmentOptimizer/GUI.java
+++ MHW Equipment Optimizer/src/equipmentOptimizer/GUI.java
@@ -3,14 +3,20 @@ package equipmentOptimizer;
 import javax.swing.*;
 
 public class GUI {
-	private EquipmentOptimizer equipmentOptimizer;
-	private JFileChooser fileChooser;
 	private JPanel mainPanel;
-	private JTextArea resultTextArea;
 	private JButton chooseFileButton;
+	private JButton startMatchingButton;
+	private JLabel eventLabel;
+	private JTextArea resultTextArea;
+
+	private EquipmentOptimizer equipmentOptimizer;
+	private String fileName;
+	private JFileChooser fileChooser;
 
 	private GUI() {
+		startMatchingButton.setEnabled(false);
 		resultTextArea.setEditable(false);
+
 		equipmentOptimizer = new EquipmentOptimizer(resultTextArea);
 
 		fileChooser = new JFileChooser();
@@ -18,19 +24,28 @@ public class GUI {
 		fileChooser.setMultiSelectionEnabled(false);
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
-
 		chooseFileButton.addActionListener(e -> {
-			String fileName;
+			startMatchingButton.setEnabled(false);
 			if (fileChooser.showOpenDialog(mainPanel) == JFileChooser.APPROVE_OPTION) {
 				fileName = fileChooser.getSelectedFile().getAbsolutePath();
+				eventLabel.setText("目前選擇檔案：" + fileName);
 
 				try {
+					startMatchingButton.setEnabled(true);
 					equipmentOptimizer.readRequirement(fileName);
 					equipmentOptimizer.generateIncludedEquipmentList();
-					equipmentOptimizer.findAndPrintMatchingEquipmentList();
-				} catch (CloneNotSupportedException cse) {
-					System.out.println(cse.getMessage());
+				} catch (Exception e1) {
+					startMatchingButton.setEnabled(false);
+					eventLabel.setText("檔案格式錯誤");
 				}
+			}
+		});
+		startMatchingButton.addActionListener(e -> {
+			try {
+				equipmentOptimizer.findAndPrintMatchingEquipmentList();
+			} catch (Exception e1) {
+				startMatchingButton.setEnabled(false);
+				eventLabel.setText("未知錯誤");
 			}
 		});
 	}
